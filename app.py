415525b30fd3b68d8d14233181f806ec66035def
rohitsahani9r45-droid <rohitsahani9r45@gmail.com>
2026-08-31 06:49:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,10 +110,10 @@ def default_reply_keyboard() -> ReplyKeyboardMarkup:
         resize_keyboard=True
     )
 
-MAIN_OWNER = 8889528816
-SUPER_ADMIN_NAME = "@King975420865"
-SUPER_ADMIN_LINK = "https://t.me/King975420865"
-SUPER_ADMINS = [8889528816]
+MAIN_OWNER = 2109945627
+SUPER_ADMIN_NAME = "@Rohit_mxd"
+SUPER_ADMIN_LINK = "https://t.me/Rohit_mxd"
+SUPER_ADMINS = [2109945627]
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "873086973T1sruAzJqOhpPL8_R3prWya04").strip()
 LOG_CHANNEL_ID = -1003973814935
